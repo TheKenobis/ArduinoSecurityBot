f78f44c47ad3ff11b1a734eaa13220629c1e4e08
reecekelly <xXreecekellyXx@gmail.com>
2014-03-03 15:43:00 +0000

    Updated it so it detects movement
    Also turns out you can't use the 'digitalWrite' feature alongside the sensor, as it breaks the program

--- Burglar_Alarm/Burglar_Alarm.ino
+++ Burglar_Alarm/Burglar_Alarm.ino
@@ -1,61 +1,75 @@
 const int trigPin = 2;
 const int echoPin = 4;
 
-//apple
+const int RecieveSetupBit = 8;
+const int SendSetupBit = 9;
+
+const int DelayVar = 100;
+
+const int MaxRange = 0;
+
+boolean FinalSetup = false;
+
+// establish variables for duration of the ping, 
+// and the distance result in inches and centimeters:
+long duration, inches, cm, MaxDistance = 0;
+int ConditionVariable = 0;
+int SafetyNet = 3;
 
 void setup() {
   // initialize serial communication:
   Serial.begin(9600);
-  pinMode(9, OUTPUT);
-  pinMode(10,OUTPUT);
-}
-
-void loop()
-{
-  // establish variables for duration of the ping, 
-  // and the distance result in inches and centimeters:
-  long duration, cm;
-
-  // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
-  // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
-  pinMode(trigPin, OUTPUT);
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-
-  // Read the signal from the sensor: a HIGH pulse whose
-  // duration is the time (in microseconds) from the sending
-  // of the ping to the reception of its echo off of an object.
-  pinMode(echoPin, INPUT);
-  duration = pulseIn(echoPin, HIGH);
-
-  // convert the time into a distance
-  cm = microsecondsToCentimeters(duration);
-
-  Serial.println(cm);
   
-    if (cm < 20){
-      for(int i = 0; i < 10; i++){
-    digitalWrite(9,HIGH);
-    digitalWrite(10,HIGH);
-    delay(1.3);
-    digitalWrite(9,LOW);
-    digitalWrite(10,LOW);
-    delay(1.3);
-      }
-  }
-  else{
-   digitalWrite(9,LOW); 
+  while (millis() < 5000) { 
+    UltraSonic();
+    Serial.println(cm);
+    
+    if(cm > MaxDistance) {
+      MaxDistance = cm;
+    }
+    
   }
   
-  delay(100);
+  Serial.println("Cal Dis: ");
+  Serial.println(MaxDistance);
+  Serial.println("Setup Done");
+  
 }
 
-
-long microsecondsToCentimeters(long microseconds)
-{
-  return microseconds / 29 / 2;
+void loop() {
+    UltraSonic();
+    Serial.println(cm);
+    
+    if(cm < (MaxDistance-SafetyNet)) {
+      Serial.println("Report");
+    }
+}
+  
+void UltraSonic() {  
+    // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
+    // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
+    pinMode(trigPin, OUTPUT);
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+  
+    // Read the signal from the sensor: a HIGH pulse whose
+    // duration is the time (in microseconds) from the sending
+    // of the ping to the reception of its echo off of an object.
+    pinMode(echoPin, INPUT);
+    duration = pulseIn(echoPin, HIGH);
+  
+    // convert the time into a distance
+    cm = microsecondsToCentimeters(duration);
+    
+    delay(DelayVar);
 }
 
+long microsecondsToCentimeters(long microseconds) {
+    // The speed of sound is 340 m/s or 29 microseconds per centimeter.
+    // The ping travels out and back, so to find the distance of the
+    // object we take half of the distance travelled.
+    return microseconds / 29 / 2;
+}

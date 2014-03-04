89d46fe9770df240182a5397a29dbb8921b82bfd
Andrew <d.a.garden@hotmail.co.uk>
2014-03-04 12:08:34 +0000

    Revert "Code Additions 2.0"
    This reverts commit 64819086d7593bf9b39653b54d1041a5c50bc569.

--- Email/src/ExtraCodeAdditions.java
+++ Email/src/ExtraCodeAdditions.java
@@ -8,10 +8,3 @@ Provide Email User ID for Email Authentication:
  
 props.setProperty("mail.user", "myuser");
  props.setProperty("mail.password", "mypwd");
-
-Email time and date stampe:
-
-Date date = new Date();
-SimpleDateFormat sdf = new SimpleDateFormat("MM/dd/yyyy h:mm:ss a");
-String formattedDate = sdf.format(date);
-System.out.println(formattedDate); // 12/01/2011 4:48:16 PM

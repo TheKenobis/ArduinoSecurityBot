ff500beb84b3c6105d0169177cf35978ed256281
Andrew <d.a.garden@hotmail.co.uk>
2014-03-04 12:15:22 +0000

    Updated SendEmail.java

--- Email/src/SendEmail.java
+++ Email/src/SendEmail.java
@@ -1,19 +1,12 @@
 /**
- * @author      Andrew Spackman <s1309454@connect.glos.ac.uk>
- * @version     0.1                 (current version number of program)
- * @since     25 Feb 2014
+ * @authors		Andrew Spackman, Josh Ravenscroft	
+ * @version		0.2 - 04/03/2014
  */
-
-//Version 1
-
-import glos.*;
 
 import java.io.BufferedReader;
 import java.io.OutputStream;
 import java.util.Date;
 import java.util.Properties;
-import java.util.Scanner;
-
 import javax.mail.Message;
 import javax.mail.Session;
 import javax.mail.Transport;
@@ -24,15 +17,13 @@ public class SendEmail {
 
 static BufferedReader input;
 	
-	int test = 0;
-	
     static OutputStream output;
     
-    static String from = "";
-    static String password = "";
-    static String[] to = { "s1309454@connect.glos.ac.uk" }; // list of recipient email addresses
-    static String host = "";
-    static String portformail = "";
+    static String from = "s1309454@gmail.com";					// Sender's email address. Currently using my (Andrew's) email account.
+    static String password = "6v,.6v,.6v,.";					// Password of the sender.
+    static String[] to = { "s1309454@connect.glos.ac.uk" }; 	// Recipient Email Address. This can be assigned multiple values
+    static String host = "smtp.gmail.com";						// For gmail, use "smtp.gmail.com" For yahoo, use "smtp.mail.yahoo.com"
+    static String portformail = "465";							// Port should be 465 if from within University. Otherwise, use 587
     
     
 	 private static void sendFromGMail(String host, String port, String from, String pass, String[] to, String subject, String body) {
@@ -49,7 +40,6 @@ static BufferedReader input;
 	            message.setFrom(new InternetAddress(from));
 	            InternetAddress[] toAddress = new InternetAddress[to.length];
 
-	            // To get the array of addresses
 	            for( int i = 0; i < to.length; i++ ) {
 	                toAddress[i] = new InternetAddress(to[i]);
 	            }
@@ -64,40 +54,21 @@ static BufferedReader input;
 	            transport.connect(host, from, pass);
 	            transport.sendMessage(message, message.getAllRecipients());
 	            transport.close();
-	            System.out.println("Email sent successfully");
-	        }
+	        	}
+	        
 	        catch (Exception ex) {
 	            ex.printStackTrace();
 	        }
 	        
-	    }
+	 }
 	 
 public static void main(String[] args) throws Exception{
     	
-    	Scanner sc= new Scanner(System.in);
-        //System.out.println("Enter user name");
-        //from = sc.next();
-        from = IO.readString("Enter Email Address of Sender");
-        
-        //System.out.println("Enter password");
-        //password = sc.next();
-        password = IO.readString("Enter Password");
-        
-        //System.out.println("Enter host, For gmail enter smtp.gmail.com, For yahoo enter smtp.mail.yahoo.com");
-        //host = sc.next();
-        host = IO.readString("Enter host, For gmail enter smtp.gmail.com, For yahoo enter smtp.mail.yahoo.com");
-        
-        //System.out.println("Enter port for mail. 465 if from within university otherwise try 587");
-        //portformail = sc.next();
-        portformail = IO.readString("Enter port for mail. 465 if from within university otherwise try 587");
-        
-        //to = IO.readString("Enter Email Address of Recipient");
+	Date date = new Date();
     
-     String subject = "CT4003 Email Test" ;
-     String body = "I'mma go first! " + new Date().getTime();
-     sendFromGMail(host, portformail, from, password, to, subject, body);
-     
-     sc.close();
+		String subject = "Security Breach Notification" ;
+		String body = "Security was breached on " + (date.toString());
+		sendFromGMail(host, portformail, from, password, to, subject, body);
      
 	}
 

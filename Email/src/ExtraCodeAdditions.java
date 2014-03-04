30201831d53aebf5adc79b21aedff524f039cddd
Josh Ravenscroft <jravenscroft12345678@gmail.com>
2014-03-04 11:30:30 +0000

    Code Additions
    Multiple Recipients
    Email User Authentication

---
+++ Email/src/ExtraCodeAdditions.java
@@ -0,0 +1,10 @@
+
+Send to Multiple Recipents:
+void addRecipients(Message.RecipientType type, 
+                   Address[] addresses)
+throws MessagingException
+
+Provide Email User ID for Email Authentication:
+ 
+props.setProperty("mail.user", "myuser");
+ props.setProperty("mail.password", "mypwd");

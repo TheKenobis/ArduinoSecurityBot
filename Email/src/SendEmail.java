ed093ceecd96c3f66a0cd8ba17037375fdabfa84
Andrew <d.a.garden@hotmail.co.uk>
2014-03-11 13:01:39 +0000

    Updated Java Code
    Java Code updated. Still having problems with Arduino connection.

--- Email/src/SendEmail.java
+++ Email/src/SendEmail.java
@@ -1,12 +1,18 @@
 /**
  * @authors		Andrew Spackman, Josh Ravenscroft	
- * @version		0.2 - 04/03/2014
+ * @version		0.3 - 11/03/2014
  */
 
-import java.io.BufferedReader;
+import gnu.io.CommPortIdentifier;
+import gnu.io.SerialPort;
+
+import java.io.Flushable;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.util.Date;
 import java.util.Properties;
+import java.util.Scanner;
+
 import javax.mail.Message;
 import javax.mail.Session;
 import javax.mail.Transport;
@@ -15,8 +21,7 @@ import javax.mail.internet.MimeMessage;
 
 public class SendEmail {
 
-static BufferedReader input;
-	
+	static InputStream input;
     static OutputStream output;
     
     static String from = "s1309454@gmail.com";					// Sender's email address. Currently using my (Andrew's) email account.
@@ -25,8 +30,44 @@ static BufferedReader input;
     static String host = "smtp.gmail.com";						// For gmail, use "smtp.gmail.com" For yahoo, use "smtp.mail.yahoo.com"
     static String portformail = "465";							// Port should be 465 if from within University. Otherwise, use 587
     
-    
-	 private static void sendFromGMail(String host, String port, String from, String pass, String[] to, String subject, String body) {
+	 public static void readFromArduino() throws Exception{
+			
+			CommPortIdentifier portId = CommPortIdentifier.getPortIdentifier("COM5");
+			
+			SerialPort port = (SerialPort)portId.open("serial talk", 4000);
+			input = port.getInputStream();
+			port.setSerialPortParams(9600,
+					SerialPort.DATABITS_8,
+					SerialPort.STOPBITS_1,
+					SerialPort.PARITY_NONE);
+	 
+			while (true) {
+				if (input.available()>0) {
+					System.out.print((char)(input.read()));
+					
+					}
+				}
+	 }
+
+	 
+		public static void sendToArduino (int command) {
+			try {
+				CommPortIdentifier portId = CommPortIdentifier.getPortIdentifier("COM5");
+				SerialPort port = (SerialPort)portId.open("serial talk", 4000);
+				output = port.getOutputStream();
+				port.setSerialPortParams(9600,
+						SerialPort.DATABITS_8,
+						SerialPort.STOPBITS_1,
+						SerialPort.PARITY_NONE);
+				
+					output.flush();
+				port.close();
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			}
+		
+	 public static void sendFromGMail(String host, String port, String from, String pass, String[] to, String subject, String body) {
 	    	Properties props = System.getProperties();
 	        props.put("mail.smtp.host", host);
 	        props.put("mail.smtp.port", port);
@@ -61,15 +102,34 @@ static BufferedReader input;
 	        }
 	        
 	 }
-	 
+		
 public static void main(String[] args) throws Exception{
     	
 	Date date = new Date();
-    
-		String subject = "Security Breach Notification" ;
-		String body = "Security was breached on " + (date.toString());
-		sendFromGMail(host, portformail, from, password, to, subject, body);
-     
-	}
 
-}
+		//String subject = "Security Breach Notification" ;
+		//String body = "Security was breached on " + (date.toString());
+		//sendFromGMail(host, portformail, from, password, to, subject, body);
+
+	while (true) {
+		
+		//Scanner reader = new Scanner(System.in);
+        //System.out.print(input);
+
+		try {
+
+		  Thread.sleep(1000L);    // one second
+
+		}
+
+		catch (Exception e) {}     // this never happen... nobody check for it
+
+        int command = 6;
+	    sendToArduino(command);
+        System.out.println(command);
+        
+		}
+	    
+		}
+	    
+	}

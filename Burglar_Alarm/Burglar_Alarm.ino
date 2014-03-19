50902e4b6aee03b3e76e6d97c3538117ecd48053
reecekelly <xXreecekellyXx@gmail.com>
2014-03-19 20:10:14 +0000

    Almost finished code
    Large amounts of the problems have been fixed, including the communication between the hardware module and java program. Added small delays within the code, to prevent code skipping. Along with fixing the calibration phase.

--- Burglar_Alarm/Burglar_Alarm.ino
+++ Burglar_Alarm/Burglar_Alarm.ino
@@ -6,105 +6,92 @@ const int RecieveSetupBit = 6;
 const int ChangeinSensor = 7;
 const int HardwareError = 8;
 
-const int DelayVar = 100;
-
+const int DelayVar = 2000;
+const int DelaySec = 100;
 const int MaxRange = 0;
 
 boolean FinalSetup = false;
 
-// establish variables for duration of the ping, 
-// and the distance result in inches and centimeters:
-long duration, inches, cm, MaxDistance = 0;
+long duration, cm, MaxDistance = 0;
 int SafetyNet = 3;
 
 void setup() {
-  // initialize serial communication:
-  Serial.begin(9600);
+  Serial.begin(9600);                     //Serial bit rate at 9600, java needs to be the same
 
-  for(int i=8; i<11; i++) {
+  for(int i=8; i<11; i++) {                //for loop setting up the lights to output
      pinMode(i, OUTPUT);
      digitalWrite(i, LOW);
+  }  
+  
+  digitalWrite(8, HIGH);  
+  
+  while(FinalSetup == false) {              //while loop which prevents the arduino from starting, before it is connected to the java program
+     Serial.print(SendSetupBit);
+     delay(DelayVar); 
+     
+     int IncomingByte = Serial.read()-'0';
+     if(IncomingByte == RecieveSetupBit) {
+       FinalSetup = true;
+     }
+      
   }
-  digitalWrite(8, HIGH);
-  Serial.print(9);
-  while(FinalSetup == false) {
+  
+ digitalWrite(8, LOW);
+ digitalWrite(9, HIGH);
+ digitalWrite(10, LOW);
+ delay(1);
+ 
+ Calibration();
 
-    if(Serial.available() > 0) {            //needs serial communication for it to work, either java or serial monitor
-      int IncomingByte = Serial.read()-'0';
-    
-      if(IncomingByte != RecieveSetupBit) {
-        Serial.println(SendSetupBit);
-      
-      } else {
-        FinalSetup = true;
-      } 
-    
-      delay(DelayVar);
-      
-    }  else {
-      Serial.println("ERROR..............");
-    } 
-    
-  } 
+ digitalWrite(8, LOW);
+ digitalWrite(9, LOW);
+ digitalWrite(10, HIGH);
+ 
+delay(2000);
+
+}
+
+void loop() {
+  UltraSonic();
+    delay(1);
+  Serial.println(cm);
   
-  digitalWrite(8, LOW);
-  digitalWrite(9, HIGH);
   
+  if(cm < (MaxDistance-SafetyNet)) { //code when enemy is near
+    Serial.println(ChangeinSensor); 
+    digitalWrite(8, HIGH);
+  digitalWrite(9, LOW);  
+    digitalWrite(10, LOW);  
+    
+    //delay has been introduced to aviod spam
+    delay(10000);
+      
+  } else if(cm <= 0){
+    Serial.println(HardwareError);
+    //delay(DelaySec);
+      
+  } else{
+      digitalWrite(8, LOW);
+  digitalWrite(9, LOW);  
+    digitalWrite(10, HIGH);  
+
+  
+  }
+}
+
+void Calibration() {
   
   while(millis() < 8000) { 
     UltraSonic();
-    Serial.println(cm);
+    //Serial.println(cm);
     
     if(cm > MaxDistance) {
       MaxDistance = cm;
     }
       
   }
-  
-  digitalWrite(9, LOW);
-  digitalWrite(10, HIGH);
-
 }
 
-void loop() {
-  
-    Serial.println(cm);
-    Serial.println(MaxDistance);
-    Serial.println(" ");
-    delay(1000);
-    
-    if(Serial.available() > 0) {  //needs serial communication for it to work, either java or serial monitor
-      
-      Serial.flush();
-      
-      digitalWrite(8, LOW);
-      digitalWrite(9, LOW);
-      digitalWrite(10, HIGH);
-  
-      UltraSonic();
-      Serial.println(cm);
-      
-      if(cm < (MaxDistance-SafetyNet)) {
-        Serial.println("Report");
-        Serial.print(ChangeinSensor);
-      
-      } else if(cm <= 0){
-        Serial.print(HardwareError);
-      
-      } else {
-        digitalWrite(8, HIGH);
-        digitalWrite(9, LOW);
-        digitalWrite(10, LOW);
-      }
-      
-    digitalWrite(12, LOW);
-    delay(100);
-    
-    }  else {
-      Serial.println("ERROR..............");
-    }
-}
-  
 void UltraSonic() {  
     // The sensor is triggered by a HIGH pulse of 10 or more microseconds.
     // Give a short LOW pulse beforehand to ensure a clean HIGH pulse:
@@ -124,7 +111,7 @@ void UltraSonic() {
     // convert the time into a distance
     cm = microsecondsToCentimeters(duration);
     
-    delay(DelayVar);
+    //delay(DelayVar);
 }
 
 long microsecondsToCentimeters(long microseconds) {
